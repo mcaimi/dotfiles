06d7931da28d6998b65c1dce16ae7a1eca774bb5
Marco Caimi <caimi.marco@protonmail.ch>
2020-07-24 15:20:06 +0200

    Updated i3 config

--- config/i3scripts/globals.sh
+++ config/i3scripts/globals.sh
@@ -29,6 +29,6 @@ KEYS=(["github"]="id_rsa_github" ["openstack"]="id_rsa_openstack" ["ansible"]="i
 
 # networkmanager and other applets that should appear in the systray
 declare -A APPLETS
-APPLETS=(["COMPTRAY"]="comptray" ["MEGASYNC"]="megasync" ["PROTONMAILBRIDGE"]="protonmail-bridge")
+APPLETS=(["MEGASYNC"]="megasync" ["PROTONMAILBRIDGE"]="protonmail-bridge")
 
 
