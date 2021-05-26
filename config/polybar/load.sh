bf8c68e850e5c84e70cdeca78b9f6c90b77eac84
Marco Caimi <caimi.marco@protonmail.ch>
2021-05-26 14:30:01 +0200

    Updated polybar configuration

--- config/polybar/load.sh
+++ config/polybar/load.sh
@@ -29,14 +29,14 @@ info "[$(date)]: Killed running polybars." $LOGFILE
 is_lid_closed
 if [ $? -ne 0 ]; then 
   info "[$(date)]: INTERNAL DISPLAY: Loading polybars...." $LOGFILE
-  polybar -c $HOME/.config/polybar/config.ini main &
+  polybar -c $HOME/.config/polybar/config.ini -q main &
 fi
 
 # Launch bar top & bottom external display
 is_display_connected HDMI2
 if [ $? -eq 0 ]; then
   info "[$(date)]: EXTERNAL DISPLAY: Loading polybars...." $LOGFILE
-  polybar -c $HOME/.config/polybar/config-ext.ini main-ext &
+  polybar -c $HOME/.config/polybar/config-ext.ini -q main-ext &
 fi
 
 info "[Polybar] Bars launched." $LOGFILE
