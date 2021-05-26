464964b407157dbbd854c7d51211566d0a3cc2d5
Marco Caimi <caimi.marco@protonmail.ch>
2021-05-26 15:33:41 +0200

    Wifi script update

--- config/i3scripts/wifi.sh
+++ config/i3scripts/wifi.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+ROFICONFIG="$HOME/.config/rofi"
+
 function _rofi {
-    rofi -location 3 -show combi -padding 2 -xoffset -5 -yoffset 30 -bw 0 -lines 25 -width 35 -font "iosevka term medium 11" -tokenize -no-levenshtein-search -dmenu "$@" -p "wireless >"
+    rofi -location 3 -show combi -lines 5 -width 35 -theme $ROFICONFIG/menu.rasi -tokenize -no-levenshtein-search -dmenu "$@" -p "wireless >"
 }
 
 OFI_MENU_CONTENTS+="---\n"
@@ -10,7 +12,7 @@ OFI_MENU_CONTENTS+="---\n"
 while read input_line
 do
    ROFI_MENU_CONTENTS+="$input_line\n"
-done <<< "$(wpa_cli list_networks)"
+done <<< "$(nmcli con | awk '/wifi/ {print $1}')"
 
 ROFI_MENU_CONTENTS+="---\n"
 ROFI_MENU_CONTENTS+="Select a wireless network to connect to.\n"
@@ -18,5 +20,5 @@ ROFI_MENU_CONTENTS+="Select a wireless network to connect to.\n"
 WIFI_STATION="$(echo -e $ROFI_MENU_CONTENTS|_rofi)"
 
 NET_ID="$(echo $WIFI_STATION|awk '{print $1}')"
-wpa_cli select_network $NET_ID
+nmcli con up NET_ID
 
