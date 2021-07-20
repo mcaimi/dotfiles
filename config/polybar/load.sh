007de62a03bc6d088e24f35e3394845b8f44dfc8
mcaimi <caimi.marco@protonmail.ch>
2021-07-20 11:59:13 +0200

    New polybar

--- config/polybar/load.sh
+++ config/polybar/load.sh
@@ -25,11 +25,18 @@ fi
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 info "[$(date)]: Killed running polybars." $LOGFILE
 
+# Launch bars on internal display if lid is open
+is_lid_closed
+if [ $? -ne 0 ]; then 
+  info "[$(date)]: INTERNAL DISPLAY: Loading polybars...." $LOGFILE
+  polybar -c $HOME/.config/polybar/config.ini -q main &
+fi
+
 # Launch bar top & bottom external display
 is_display_connected DisplayPort-0
 if [ $? -eq 0 ]; then
   info "[$(date)]: EXTERNAL DISPLAY: Loading polybars...." $LOGFILE
-  polybar -c $HOME/.config/polybar/config-ext.ini main-ext &
+  polybar -c $HOME/.config/polybar/config-ext.ini -q main-ext &
 fi
 
 info "[Polybar] Bars launched." $LOGFILE
