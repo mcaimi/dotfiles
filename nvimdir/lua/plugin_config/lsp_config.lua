b2b8a1065591fdd3f8a3f2b47e41b68afb047ada
mcaimi <caimi.marco@protonmail.ch>
2021-11-09 12:38:32 +0100

    Updated NeoVim lua configuration

--- nvimdir/lua/plugin_config/lsp_config.lua
+++ nvimdir/lua/plugin_config/lsp_config.lua
@@ -1,45 +1,45 @@
 -- configuration for LSPs
-local lspcfg = require('lspconfig')
-local lspinst = require('lspinstall')
-local utils = require('utils.utils')
+local lspinst = require('nvim-lsp-installer')
+local lspservers = require('nvim-lsp-installer.servers')
+local utils = require('utils.tables')
 local M = {}
 
--- declare which servers need to be installed/updated on load
-local lang_list = {
-  'cpp',
-  'dockerfile',
-  'bash',
-  'yaml',
-  'lua',
-  'go',
+-- custom settings
+lspinst.settings {
+    ui = {
+        icons = {
+            server_installed = "✓",
+            server_pending = "➜",
+            server_uninstalled = "✗"
+        }
+    }
 }
 
--- setup lspinstall plugin
-lspinst.setup()
+-- declare which servers need to be installed/updated on load
+local lang_list = utils.supported_languages
 
 -- install or upgrade servers
 local function install_lsp_servers()
-  local is = lspinst.installed_servers()
   for _, language in pairs(lang_list) do
-    if not utils.contains(is, language) then
-      lspinst.install_server(language)
+    local ok, server = lspservers.get_server(language)
+    if ok then
+      if not server:is_installed() then
+        server:install()
+      end
     end
   end
 end
 
--- call lspconfig for each LSP installed
-local function setup_lsp_servers()
-  for _, server in pairs(lspinst.installed_servers()) do
-      lspcfg[server].setup{}
-  end
-end
+-- setup lsp plugin
+lspinst.on_server_ready(function(server)
+  local opts = {}
 
--- install and configure lsp servers
-setup_lsp_servers()
+  server:setup(opts)
+  vim.cmd [[ do User LspAttachBuffers ]]
+end)
 
--- setup post-install hook: setup servers on every update
-lspcfg.post_install_hook = function () setup_lsp_servers() end
-
--- return public functions
+-- exported symbols
+M.required_language_servers = lang_list
 M.install_lsp_servers = install_lsp_servers
+
 return M
