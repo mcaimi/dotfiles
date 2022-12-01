8764159c07d1e80dbec7611064304e419db9adec
Marco Caimi <mcaimi@redhat.com>
2022-12-01 17:33:23 +0100

    Moved & reworked bash dotfiles

--- fzf.bash
+++ fzf.bash
@@ -1,4 +1,3 @@
-# FZF settings for vsrious tools
-export SKIM_DEFAULT_OPTIONS="--ansi --height=25 --layout=reverse --margin=TRBL --prompt='SKIM > ' --header='Indexing $PWD'"
-bind -m vi -x '"\C-f": "code_file=$( find $PWD -type f | rg -f ~/Work/dotfiles/codefile-patterns | sk-tmux ); [ $? -eq 0  ] && vim $code_file"'
+# FZF settings for various tools
+bind -m vi -x '"\C-f": "code_file=$( find $PWD -type f | rg -f ~/Work/dotfiles/codefile-patterns | fzf-tmux -h ); [ $? -eq 0  ] && vim $code_file"'
 
